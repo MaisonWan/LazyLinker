3be9105e658548ea8182d26eb858465f130d401d
Maison Wan <maisonwan@gmail.com>
2018-01-17 01:54:20 +0800

    修改工程配置为gradle

--- src/com/domker/lazylinker/data/LinkerParser.kt
+++ src/com/domker/lazylinker/data/LinkerParser.kt
@@ -1,5 +1,9 @@
 package com.domker.lazylinker.data
 
+import com.google.gson.Gson
+import java.io.File
+import java.io.FileReader
+
 class LinkerParser {
 
     /**
@@ -11,5 +15,30 @@ class LinkerParser {
         list.add(LinkerPage("拷贝"))
         list.add(LinkerPage("安装"))
         return list
+    }
+
+//    private fun parserJson(data: String) {
+//        val data = readJsonData()
+//        data?.let {
+//            val gson = Gson()
+//            val d = gson.fromJson(data, LinkerPage::class.java)
+//
+//        }
+//    }
+
+    /**
+     * 读取配置文件
+     */
+    private fun readJsonData(): String? {
+        var reader: FileReader? = null
+        return try {
+            val reader = FileReader(File("./linker.json"))
+            reader.readLines().reduce { acc, s -> acc + s }
+        } catch (e: Exception) {
+            null
+        } finally {
+            reader?.close()
+        }
+
     }
 }
