4e288282547f691d2d865a3a35bd0efa5effed60
Maison Wan <maisonwan@gmail.com>
2018-01-17 11:37:58 +0800

    更新了gradle的配置

--- src/main/kotlin/com/domker/lazylinker/data/LinkerParser.kt
+++ src/main/kotlin/com/domker/lazylinker/data/LinkerParser.kt
@@ -1,5 +1,6 @@
 package com.domker.lazylinker.data
 
+//import com.google.gson.Gson
 import com.google.gson.Gson
 import java.io.File
 import java.io.FileReader
@@ -10,21 +11,23 @@ class LinkerParser {
      * 解析
      */
     fun parser(): List<LinkerPage> {
-        val list = mutableListOf<LinkerPage>()
+        val data = readJsonData()
+        val list = parserJson(data) as MutableList //mutableListOf<LinkerPage>()
         list.add(LinkerPage("编译"))
         list.add(LinkerPage("拷贝"))
         list.add(LinkerPage("安装"))
         return list
     }
 
-//    private fun parserJson(data: String) {
-//        val data = readJsonData()
-//        data?.let {
-//            val gson = Gson()
-//            val d = gson.fromJson(data, LinkerPage::class.java)
-//
-//        }
-//    }
+    private fun parserJson(data: String?): List<LinkerPage> {
+        val linkerPages = mutableListOf<LinkerPage>()
+        data?.let {
+            val gson = Gson()
+            val d = gson.fromJson(data, LinkerPage::class.java)
+            linkerPages.add(d)
+        }
+        return linkerPages
+    }
 
     /**
      * 读取配置文件
@@ -32,7 +35,7 @@ class LinkerParser {
     private fun readJsonData(): String? {
         var reader: FileReader? = null
         return try {
-            val reader = FileReader(File("./linker.json"))
+            reader = FileReader(File("./linker.json"))
             reader.readLines().reduce { acc, s -> acc + s }
         } catch (e: Exception) {
             null
